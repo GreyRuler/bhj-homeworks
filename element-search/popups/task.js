f24c46dfa4ca0e3621a46c695d654ec3482a141f
GreyRuler <daniil_vasilenko757@mail.ru>
2022-07-17 22:32:30 +0300

    Fix popups

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,8 +1,18 @@
 const modalMain = document.getElementById('modal_main');
+const modalSuccess = document.getElementById('modal_success');
 
 modalMain.className = "modal modal_active";
 
-const button = document.querySelector('.modal__close');
-button.onclick = () => {
+const btnDanger = document.querySelector('.btn_danger')
+btnDanger.onclick = () => {
     modalMain.className = "modal";
+    modalSuccess.className = "modal modal_active";
 }
+
+const buttonClose = [...document.querySelectorAll('.modal__close_times')];
+buttonClose.forEach(item =>
+    item.onclick = () => {
+        modalMain.className = "modal";
+        modalSuccess.className = "modal";
+    }
+)
