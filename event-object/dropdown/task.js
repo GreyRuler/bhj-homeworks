8bc239aa2b25f08888db0cb1f05396e4dfa6c62c
GreyRuler <daniil_vasilenko757@mail.ru>
2022-07-18 22:05:18 +0300

    mpl dropdown

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,22 @@
+const dropdowns = [...document.querySelectorAll('.dropdown')];
+
+dropdowns.forEach(item => {
+    const dropdown = item.querySelector('.dropdown__value')
+    const dropdownList = item.querySelector('.dropdown__list')
+    const dropdownItems = item.querySelectorAll('.dropdown__item ')
+
+    dropdownItems.forEach(element => {
+        element.addEventListener('click', function(event) {
+            dropdown.textContent = this.textContent
+            dropdownList.className = 'dropdown__list'
+            event.preventDefault()
+        })
+    })
+    dropdown.addEventListener('click', function() {
+        if (dropdownList.className == 'dropdown__list dropdown__list_active') {            
+            dropdownList.className = 'dropdown__list'
+            return
+        }
+        dropdownList.className += ' dropdown__list_active'
+    })
+});
