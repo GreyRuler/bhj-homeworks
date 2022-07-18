c0e2c8e4d3a8c9d4716a08e9e3197de73e962425
GreyRuler <daniil_vasilenko757@mail.ru>
2022-07-18 19:25:31 +0300

    ref

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -3,23 +3,25 @@ const dead = document.getElementById("dead");
 const lost = document.getElementById("lost");
 const children = [...holeGame[0].children];
 
+function resetStatistics(text) {
+    lost.textContent = 0;
+    dead.textContent = 0;
+    alert(text);
+}
+
 children.forEach(element => {
     element.onclick = () => {
         if (element.className == 'hole') {
             let lostNumber = Number.parseInt(lost.textContent);
             lost.textContent = lostNumber + 1;
             if (lost.textContent == 5) {
-                lost.textContent = 0;
-                dead.textContent = 0;
-                alert("Вы проиграли!");
+                resetStatistics("Вы проиграли!");
             }
         } else {
             let deadNumber = Number.parseInt(dead.textContent);
             dead.textContent = deadNumber + 1;
             if (dead.textContent == 10) {
-                lost.textContent = 0;
-                dead.textContent = 0;
-                alert("Вы выиграли!");
+                resetStatistics("Вы выиграли!");
             }
         }
     }
