5062432b9dc6d763116ace822b0c0322675f3356
GreyRuler <daniil_vasilenko757@mail.ru>
2022-07-18 19:20:03 +0300

    ref

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -4,14 +4,7 @@ const cookie = document.getElementById("cookie");
 let date1 = Date.now();
 
 cookie.onclick = function() {
-    let countCookie = Number.parseInt(count.textContent);
-    if (countCookie % 2 === 0) {
-        cookie.width = 150;
-        count.textContent = Number.parseInt(count.textContent) + 1;
-    } else {
-        cookie.width = 200;
-        count.textContent = Number.parseInt(count.textContent) + 1;
-    }
+    cookie.width = ++count.textContent % 2 ? 250 : 200;
     let date2 = Date.now()
     speed.textContent = (1 / (date2 - date1) * 1000).toFixed(2);
     date1 = date2;
