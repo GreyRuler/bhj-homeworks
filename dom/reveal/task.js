1e7bb7de9b274a3fdb74c5f380de3fa23f5ccb72
GreyRuler <daniil_vasilenko757@mail.ru>
2022-07-20 19:52:58 +0300

    Impl revel

---
+++ dom/reveal/task.js
@@ -0,0 +1,16 @@
+const reveals = [...document.querySelectorAll('.reveal')]
+
+const isInViewport = function(element) {    
+    const viewportHeight = window.innerHeight
+    const elementTop = element.getBoundingClientRect().top
+    const elementBottom = element.getBoundingClientRect().bottom
+    if (elementTop < viewportHeight && elementBottom > 0) {
+        element.classList.add('reveal_active')
+    } else {
+        element.classList.remove('reveal_active')
+    }
+}
+
+reveals.forEach(revel => {
+    setInterval(isInViewport, 100, revel)
+})
