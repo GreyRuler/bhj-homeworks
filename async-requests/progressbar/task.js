f3462de745d8bd3c908327671b08ae32512b0a05
GreyRuler <daniil_vasilenko757@mail.ru>
2022-07-31 14:18:22 +0300

    Fix

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -6,17 +6,9 @@ form.addEventListener('submit', (e) => {
     const formData = new FormData(form)
     const xhr = new XMLHttpRequest()
     xhr.open('POST', form.getAttribute('action'), true)
-    xhr.addEventListener("progress", (e) => {
-        // if (e.lengthComputable) {
-        //     console.log(e.loaded / e.total)
-        // }
-        // console.log(e.loaded)
-        console.log((e.loaded / e.total) * 100) // Infinity
-    })
-    xhr.addEventListener('readystatechange', () => {
-        console.log(xhr)
-        // progress.value += 0.7
-    })
+    xhr.upload.onprogress = function(event) {
+        progress.value =  event.loaded / event.total
+    };
     xhr.send(formData)
     e.preventDefault()
 })
